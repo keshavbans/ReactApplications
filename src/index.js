390a8d174d42c2f8157ce536a8f54af2c2cf789e
Keshav Bansal <keshavbans@gmail.com>
2018-02-24 15:10:51 +0000

    development phase 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCl9Z-uF_h7cEbY6iKqUnX9wL7fqc7zZZ4';
+
 const App = () => {
-    return <div>Hi!</div>
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    )
 }
 
-ReactDOM.render(<App />, document.querySelecto r('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
