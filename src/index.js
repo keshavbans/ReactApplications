b9c7e42da14e899a8926ef1a5444ea4ddd0c23d0
Keshav Bansal <keshavbans@gmail.com>
2018-02-25 23:35:21 +0000

    merging local changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyCl9Z-uF_h7cEbY6iKqUnX9wL7fqc7zZZ4';
+
+YTSearch({key:API_KEY, term: 'surfboards'}, function(data){
+    console.log(data);
+});
 
 const App = () => {
     return (
